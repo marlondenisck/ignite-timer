9c25ef9c1d4d117b12f314fd2cc1162f918ddcc6
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-03-05 22:48:41 -0400

    adiciona tema padrão e componente de botão estilizado em styled componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-
+import { ThemeProvider } from "styled-components";
+import { Button } from './components/Button';
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>aaaa</h1>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </ThemeProvider>
   )
 }
