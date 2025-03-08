4b55fac832786cb31f23d8be5eb98e04f1bd4f97
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-03-08 17:18:48 -0400

    adiciona configuração do Prettier e atualiza regras do ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -5,29 +5,40 @@ import tseslint from 'typescript-eslint'
 import { FlatCompat } from '@eslint/eslintrc'
 
 // Criar uma instância de compatibilidade para carregar configs tradicionais
-const compat = new FlatCompat()
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname
+})
 
 export default tseslint.config(
   { ignores: ['dist'] },
   ...compat.config({
-    extends: ['@rocketseat/eslint-config/react'],
+    extends: ['@rocketseat/eslint-config/react', 'prettier'],
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          arrowParens: 'avoid'
+        }
+      ]
+    }
   }),
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
+        { allowConstantExport: true }
+      ]
+    }
+  }
 )
---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'none',
+  endOfLine: 'auto',
+  tabWidth: 2,
+  printWidth: 80,
+  bracketSpacing: true
+}
