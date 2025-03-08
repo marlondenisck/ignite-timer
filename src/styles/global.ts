08b1b056d5e7b5fb27d7ba3bce9c254e9b10d73b
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-03-08 17:18:54 -0400

    Página: Home

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,14 +9,15 @@ export const GlobalStyle = createGlobalStyle`
 
   :focus {
     outline: none;
-    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
+    box-shadow: 0 0 0 2px ${props => props.theme['green-500']};
   }
 
   body {
     background: #333;
     color: #FFF;
-    background: ${(props) => props.theme['gray-900']};
-    color: ${(props) => props.theme['gray-300']};
+    background: ${props => props.theme['gray-900']};
+    color: ${props => props.theme['gray-300']};
+    -webkit-font-smoothing: antialiased;
   }
 
   body, input, textarea, button {
