296c47b846c94d25b733a3176b7ec11bbb3b3010
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-03-15 16:35:02 -0400

    Contexto entre rotas

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import { BrowserRouter } from 'react-router-dom'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
+import { CyclesContextProvider } from './contexts/CyclesContext'
+
 import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
