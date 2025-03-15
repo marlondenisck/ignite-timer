286a43f7eaf9040bedc85204c31800f8d6704387
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-03-15 15:19:32 -0400

    adiciona suporte ao Terser para minificação e configura opções de compressão

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'terser', // Usa o Terser no lugar do esbuild
+    terserOptions: {
+      compress: {
+        drop_console: true, // Remove todos os console.log
+        drop_debugger: true
+      },
+      mangle: true // Renomeia variáveis para códigos mais curtos
+    }
+  }
 })
